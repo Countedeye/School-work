614a4d9eab61c67829224f8a11831d1367fa72ed
Will Sutton <william_sutton@live.com>
2021-02-01 17:21:22 -0600

    homework_math_chapter1

---
+++ math_assignment_ch1.py
@@ -0,0 +1,26 @@
+# Ask the user for two numbers.                                                 
+# Quiz the user on the product, sum, difference, quotient, and remainder.       
+#                                                                               
+# Example transcript    
+
+
+input1 = input("Enter number 1:")
+input2 = input("Enter number 2:")
+sum = int(input1) + int(input2)
+print("press Enter to see answer")
+print(sum)
+diff = int(input1) - int(input2)
+print("press Enter to see answer")
+print(diff)
+mult = int(input1) * int(input2)
+print("press Enter to see answer")
+print(mult)
+div = int(input1) / int(input2)
+print("press Enter to see answer")
+print((diff))
+quot = int(input1) // int(input2)
+print("press Enter to see answer")
+print(float(quot))
+remain = int(input1) % int(input2)
+print("press Enter to see answer")
+print(float(remain))
