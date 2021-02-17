24e902d0428465ae962a072fbe013038da944f91
Will Sutton <william_sutton@live.com>
2021-02-17 11:48:43 -0600

    quiz and examples of functions.

---
+++ practiceChp3.py
@@ -0,0 +1,26 @@
+'''def sayHello(name):
+    print("hello," + name)
+
+def getInfo():
+    name=input("Enter your name: ")
+    age=input("Enter your age: ")
+    address=input("Enter you address: ")
+    print("hello," +name+ ". You are " +str(age)+ " years old and from " + address)
+
+
+print("Hi!")
+yourName=input("Enter your name: ")
+sayHello(yourName)
+for i in range(5):
+    getInfo()'''
+
+def my_function(country):
+      print("I am from " + country)
+ 
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")  
+
+
+
