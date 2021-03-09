89df18ed65b307244973a01515a355834ed52afa
Will Sutton <william_sutton@live.com>
2021-03-08 18:15:52 -0600

    cs101 assignment2

---
+++ sum.py
@@ -0,0 +1,16 @@
+# Collecting first number.
+print("Enter a number: ")
+user_data_1 = input()
+num1 = int(user_data_1)
+
+#Collecting second number.
+print("Enter another number: ")
+user_data_2 = input()
+num2 = int(user_data_2)
+
+# Adding First and second number.
+result = num1 + num2
+
+
+#answers the sum of the two chosen numbers.
+print("The sum of your chosen numbers is " + str(result) + "!")
