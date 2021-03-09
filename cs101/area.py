f9f0ad605112d421eb9ac5a3e9b5ae8cb715648e
Will Sutton <william_sutton@live.com>
2021-03-08 18:31:09 -0600

    cs101 assignment3

---
+++ cs101/area.py
@@ -0,0 +1,16 @@
+# Collecting first number.
+print("Enter a number: ")
+user_data_1 = input()
+num1 = float(user_data_1)
+
+#Collecting second number.
+print("Enter a second number: ")
+user_data_2 = input()
+num2 = float(user_data_2)
+
+# Adding First and second number.
+result = num1 * num2
+
+
+#answers the sum of the two chosen numbers.
+print("The area of your chosen numbers is " + str(result) + "!")
