21090d888d0801fc25675d8bc22b47cfb6048534
Will Sutton <william_sutton@live.com>
2021-04-25 14:31:39 -0500

    lab4

---
+++ cs101/flip_if.py
@@ -0,0 +1,19 @@
+import random
+
+
+def flip(x, y):
+    outcomes = [x, y]
+    #variable that stores answer.
+    result = random.choice(outcomes)
+    print(f"The coin landed on {result}")
+    return result
+
+
+guess = input("Guess the result. Type either \"heads\" or \"tails\" and press ENTER:\n> ")
+guess = guess.strip().lower()
+flipResult = flip("heads","tails")
+flipResult = flipResult.strip().lower()
+if guess == flipResult:
+    print("You win!")
+else:
+    print("Sorry you lose...")
