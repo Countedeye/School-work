13f39e338a94facda3e0070474b934e81707886e
Will Sutton <william_sutton@live.com>
2021-04-24 20:47:57 -0500

    lab4

---
+++ cs101/params_and_args.py
@@ -0,0 +1,11 @@
+import random
+
+def flip(x,y):
+        outcomes = ["honda", "subaru"]
+        #variable that stores answer.
+        result = random.choice(outcomes)
+        print(result)
+
+
+#calls function "flip".
+flip("yes","no")
