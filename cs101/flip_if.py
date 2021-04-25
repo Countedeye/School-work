c5c3e1b5b20708db412772edf954922e871d0571
Will Sutton <william_sutton@live.com>
2021-04-25 14:42:45 -0500

    lab4

--- cs101/flip_if.py
+++ cs101/flip_if.py
@@ -11,7 +11,7 @@ def flip(x, y):
 
 guess = input("Guess the result. Type either \"heads\" or \"tails\" and press ENTER:\n> ")
 guess = guess.strip().lower()
-flipResult = flip("heads","tails")
+flipResult = flip("HEADS","TAILS")
 flipResult = flipResult.strip().lower()
 if guess == flipResult:
     print("You win!")
