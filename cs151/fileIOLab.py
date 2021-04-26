84fcc919dafa514a6fdddb1d88f8884ac8645d4a
Will Sutton <william_sutton@live.com>
2021-04-25 21:06:33 -0500

    changes made

--- cs151/fileIOLab.py
+++ cs151/fileIOLab.py
@@ -1,10 +1,14 @@
+# William Sutton
+# cs15120
 
 def headOfFile(myFile):
+    #searches through file from beginning line in an increment of 5.
     for x in range(5):
         print(myFile.readline())
 
 def tailOfFile(myFile):
    lineNum = 0
+   #searches the file in reverse by increment of 5.
    for line in reversed(myFile.readlines()):
         if lineNum < 5:
             print(line)
@@ -12,8 +16,11 @@ def tailOfFile(myFile):
 
 def wc(myFile):
     stringFile = myFile.read()
+    #creates array of words by spitting on whitespace.
     wordCount = stringFile.split()
+    #creates array of lines by splitting on whitepspace.
     lineCount = stringFile.splitlines()
+    #prints variables.
     print(f"Line Count = {len(lineCount)}")
     print(f"word count = {len(wordCount)}")
 
@@ -21,13 +28,18 @@ def wc(myFile):
 
 def stats(myFile):
     fileString = myFile.read()
+    #creates array of words by splitting on whitespace.
     words = fileString.split()
+    #variables that hold maximum and minimum number.
     maxNum = int(words[0])
     minNum = int(words[0])
+    #variable that holds average and number count.
     avg = 0
     numberCount = 0 
+    #for loop that searches for highest and lowest number. Also searches for avg.
     for word in words:
         numberCount += 1
+        
         if int(word) > int(maxNum):
             maxNum = word
         elif int(word) < int(minNum):
