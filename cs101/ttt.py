e98d80a7300467ca069e8f844cee95c4f2e0e60d
Will Sutton <william_sutton@live.com>
2021-05-06 07:26:08 -0500

    changes

--- cs101/ttt.py
+++ cs101/ttt.py
@@ -69,7 +69,7 @@ def checkForWinner(board,player):
     
 
     # checking tie
-    if board[0][0] != " " and board[0][1] != " " and board[0][2] == " " and board[0][1] != " " and board[1][1] != " " and board[2][1] != " " and board[2][0] != " " and board[2][1] != " " and board[2][2] == " ":
+    if board[0][0] != " " and board[0][1] != " " and board[0][2] != " " and board[1][0] != " " and board[1][1] != " " and board[1][2] != " " and board[2][0] != " " and board[2][1] != " " and board[2][2] != " " :
         return "cat"
 
 
