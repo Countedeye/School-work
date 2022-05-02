4e12153d70d7808e88d6f59e975c68834e01241a
Will Sutton <william_sutton@live.com>
2022-05-02 13:35:41 -0400

    changes

--- CS201/lab5/DoubleLinkedList.py
+++ CS201/lab5/DoubleLinkedList.py
@@ -3,39 +3,45 @@
 from DoubleLinkedNode import DoubleLinkedNode as Node
 
 class DoubleLinkedList:
+
+
     def __init__(self):
         self._length = 0
         self.start = None
         self.end = None
 
+
     def __len__(self):
         return self._length
 
+
     def _get_node(self, index):
         if index < 0 or index >= self._length:
-            raise IndexError
+                raise IndexError
         if index < self._length / 2 :
-            current_node=self.start
-            current_index= 0
+            current_node = self.start
+            current_index = 0
             while current_index < index:
-                current_node=current_node.next
+                current_node = current_node.next
                 current_index += 1
 
             return current_node
         else:
-            current_node=self.end
-            current_index= self._length - 1
+            current_node = self.end
+            current_index = self._length - 1
 
             while current_index > index:
-                current_node=current_node.prev
+                current_node = current_node.prev
                 current_index -= 1
         return current_node
         pass
+
 
     def get_value(self, index):
         return self._get_node(index).value
         # # Hint: Call `self._get_node(index)`
         pass
+
 
     def insert(self, index, val):
         if index > self._length or index < 0:
@@ -47,7 +53,7 @@ class DoubleLinkedList:
             self.start= new_node
             self.end= new_node
             self._length +=1
-            return new_node
+            return
 
         if index == 0:
             first_node = self.start
@@ -55,7 +61,7 @@ class DoubleLinkedList:
             new_node.next = first_node
             first_node.prev = new_node
             self._length +=1
-            return new_node
+            return
 
         if index == self._length:
             last_node = self.end
@@ -63,42 +69,55 @@ class DoubleLinkedList:
             new_node.prev = last_node
             last_node.next = new_node
             self._length += 1
-            return new_node
-
+            return
 
         return self._get_node(index)
-        # Hint: Use `self._get_node(index)` to get the node right before the insertion
         pass
+
 
     def append(self, val):
         index = self._length
         self.insert(index, val)
         pass
 
+
     def prepend(self, val):
         return self.insert(0, val)
         # Hint: Call `self.insert()` using `0` as the index
         pass
 
+
     def remove(self, val):
-        for x in range(self.end):
-            if Node.value == val:
-                Node.value.pop()
-        # Hint: Iterate from the start checking if `node.value == val`.
+
+        if self._length == 0:
+            return ValueError
+
+        current_node = self.start
+        current_index = 0
+
+        while current_node.value != val and current_index < self._length:
+            current_node = current_node.next
+            current_index += 1
+
+        if current_index == self._length:
+            return
+
+        return self.pop(current_index)
         pass
 
+
     def pop(self, index):
-        if index >= self.length or index< 0:
+        if index >= self._length or index < 0:
             raise IndexError
 
-        if index == 0 #removes first
-            first_node= self.start
+        if index == 0: #removes first
+            first_node = self.start
             self.start = first_node.next
             del first_node
             self._length -= 1
             return
 
-        if index == self.length -1: #removes last
+        if index == self._length -1: #removes last
             last_node = self.end
             self.end = last_node.prev
             del last_node
@@ -112,10 +131,11 @@ class DoubleLinkedList:
         before_node.next = after_node
         after_node.prev = before_node
 
-        self._length -=1
+        self._length -= 1
         del remove_node
         return
         pass
+
 
     def print_forward(self):
         print("---- DoubleLinkedList - Forward ---")
@@ -126,8 +146,9 @@ class DoubleLinkedList:
                 print(current_node.value)
                 current_node = current_node.next
                 current_index += 1
-            pass
+        pass
         print("-------------------------------------")
+
 
     def print_reverse(self):
         print("---- DoubleLinkedList - Reverse ---")
@@ -143,6 +164,27 @@ class DoubleLinkedList:
         pass
 
 
-if __name__ == "__main__":
-
-    dll = DoubleLinkedList()
+# if __name__ == "__main__":
+#
+#     dll = DoubleLinkedList()
+#
+#     a= Node("Alligator")
+#     b= Node("Bird")
+#     c= Node("Cat")
+#     d= Node("Dog")
+#     e= Node("Elephant")
+#
+#
+# a.next= b
+# b.prev=a
+#
+# b.next=c
+# c.prev=b
+# dll.start = a
+# dll.end = e
+# dll._length = 5
+#
+# dll.insert(0, "foo")
+# print(dll.get_value(0))
+# print(dll.get_value(4))
+# print(len(dll))
